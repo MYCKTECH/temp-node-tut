968a46217fb9d549667668470577e0aab24d5f94
MYCKTECH <meekieymoniey067@gmail.com>
2022-11-27 18:13:01 +0100

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,9 @@
+// GLOBAL - NO WINDOW
+
+// _dirname  - path to current directory
+// _filename - file name
+// require   - function to use modules (CommonJS)
+// module    - info about current modules (file)
+// process   - info about env where the program is being executed
+
+console.log(__dirname);
